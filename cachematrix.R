101584035b33cc957275f0c0a5e31c1a2cca7f5a
RunningMJ <mjlaye@MJs-2013-Air.local>
2014-06-22 07:56:30 -0700

    My changes for the course
    Pretty sure this is wrong, but just trying to submit so I can see what
    the correct way of doing this is.  I know its easy, but for some reason
    can’t get it to work.  Overthinking it?

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL ##removes the class of unknown object 'm'
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        } ##created vector "set" which contains the values input into makeCacheMatrix function
+        getmatrix <- function() x ##returns the values of the vector x
+        setinverse <- function(solve) m <<- solve #function to set the inverse of the classless 'm'
+        getinverse <- function() m #returns the inverse of 'm' as a function
+        list(set = set, getmatrix = getmatrix,
+             setinverse = setinverse,
+             getinverse = getinverse) #creates a list of all the function created above
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix() #places previus matrix in 'm' variable
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m) #tests whether m is the same matrix as already computed in makeCachematrix, returns if true
+        }
+        data <- x$getmatrix() #makes new matrix if different from previous
+        m <- solve(data, ...) #computes the inverse of new matrix
+        x$setinverse(m)
+        m  ## Return a matrix that is the inverse of 'x'
 }
